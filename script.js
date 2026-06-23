99bfc3154ac1733ef527b2156ce16335a128b32f
Lucian45T <155108954+Lucian45T@users.noreply.github.com>
2026-06-23 22:29:36 +0300

    Refactor bullet and explosion position calculations

--- script.js
+++ script.js
@@ -1,52 +1,63 @@
+const positionBulletintoPlane = 48;
+const positionExplosionSwapPlanePlace = 25;
+const speedFallObstacle = 10;
+const speedBullet = 10
+const maxUpBulletPosition = - 10;
+const initialObstacleVerticalPosition = - 30;
+const maxRightPosition = 380;
+const maxLeftPosition = 20;
+const maxUpPosition = 20;
+const maxDownPosition = 380;
 let initialHorizontalPosition = 200;
 let initialVerticalPosition = 300;
 let planeX = initialHorizontalPosition;
 let planeY = initialVerticalPosition;
-let bulletX = planeX + 48;
+let bulletX = planeX + positionBulletintoPlane;
 let bulletY = planeY;
-let explosionX = planeX + 25;
-let explosionY = planeY + 25;
-const speed = 10;
+let explosionX = planeX + positionExplosionSwapPlanePlace;
+let explosionY = planeY + positionExplosionSwapPlanePlace;
 
 var obstaclePositions = [35, 105, 180, 
   250, 320, 390, 435];
-var speedFall = [10, 15, 25, 20, 7];
+  
+var differentObstaclesSpeedFall = [10, 15, 25, 20, 7];
 
 const airplanePoints = [ [50, 0], [54, 5], [55, 25],
   [98, 45], [98, 55], [55, 52], [52, 73], [62, 85],   
   [62, 88], [50, 83], [38, 88], [38, 85], [48, 73], 
   [45, 52], [2, 55], [2, 45], [45, 25], [46, 5] ];
+
+const airplanePoly = new SAT.Polygon( new SAT.Vector
+  (planeX, planeY), airplanePoints.map(p => new 
+  SAT.Vector(p[0], p[1]))
+);  
      
 let hit = 0, myInterval = null;
 let numberObstacles = 0, numberPassObstacles = 0;
 explosionId.style.left = explosionX + "px";
 explosionId.style.top = explosionY + "px"; 
-const maxRightPosition = 380;
-const maxLeftPosition = 20;
-const maxUpPosition = 20;
-const maxDownPosition = 380;
 
 document.addEventListener("keydown", (e) => {
   if (hit === 0) {
-    if (e.key === "ArrowRight" && planeX <= maxRightPosition) planeX += speed; 
-    if (e.key === "ArrowLeft" && planeX >= maxLeftPosition)  planeX -= speed;
-    if (e.key === "ArrowUp" && planeY >= maxUpPosition) planeY -= speed;
-    if (e.key === "ArrowDown" && planeY <= maxDownPosition)  planeY += speed;
+    if (e.key === "ArrowRight" && planeX <= maxRightPosition) planeX += speedFallObstacle; 
+    if (e.key === "ArrowLeft" && planeX >= maxLeftPosition)  planeX -= speedFallObstacle;
+    if (e.key === "ArrowUp" && planeY >= maxUpPosition) planeY -= speedFallObstacle;
+    if (e.key === "ArrowDown" && planeY <= maxDownPosition)  planeY += speedFallObstacle;
     planeId.style.left = planeX + "px";
     planeId.style.top = planeY + "px";
   }
 });
  
 function bulletReset() {
-  bulletX = planeX + 48;
+  bulletX = planeX + positionBulletintoPlane;
   bulletY = planeY;
 }  
 
 function fire() {
-  bulletY -= speed; 
+  bulletY -= speedBullet; 
   bulletsId.style.top = bulletY + "px";
   bulletsId.style.left = bulletX + "px";
-  if (bulletY == -10) { 
+  if (bulletY == maxUpBulletPosition) { 
     clearInterval(myInterval);
     myInterval = null;
   }
@@ -67,7 +78,6 @@ function fall(name, obstacleY, intervalId, differentSpeed, copyObstacleX) {
     name.style.top = obstacleY.value + "px";
     collisionObstacles(copyObstacleX, obstacleY.value);
   }
-  
   if (obstacleY.value >= 460) {
     clearInterval(intervalId);
     name.remove(); 
@@ -86,26 +96,22 @@ function obstacleRandomPosition() {
   numberObstacles = obstaclePositions[random];
   let copyObstacleXParameter = numberObstacles;
   obstacleDiv.style.left = numberObstacles + "px";
-  obstacleDiv.style.top = "-30px";
-  let obstacleY = {value:-30};
+  obstacleDiv.style.top = initialObstacleVerticalPosition + "px";
+  let obstacleY = {value: initialObstacleVerticalPosition};
   document.getElementById("container").appendChild(obstacleDiv);
-  let delay = Math.floor(Math.random() * speedFall.length);
-  let differentSpeedFall = speedFall[delay];
+  let delay = Math.floor(Math.random() * differentObstaclesSpeedFall.length);
+  let differentSpeedFall = differentObstaclesSpeedFall[delay];
   const intervalId = setInterval(() => fall(obstacleDiv, obstacleY,
     intervalId, differentSpeedFall, copyObstacleXParameter), 100);
 
 }
-setInterval(obstacleRandomPosition, 2000);    
+setInterval(obstacleRandomPosition, 2000);     
 
 function explosionCollision() {
   document.getElementById("explosionId").classList.add("active");
   document.getElementById("planeId").classList.add("active");
   document.getElementById("obstacleId").classList.add("active"); 
 }
-
-const airplanePoly = new SAT.Polygon( new SAT.Vector
-  (planeX, planeY), airplanePoints.map(p => new 
-  SAT.Vector(p[0], p[1])));
 
 function collisionObstacles(element1, element2) {
   let newObstacleX = element1;
@@ -126,15 +132,16 @@ function collisionObstacles(element1, element2) {
   const response = new SAT.Response(); 
   const collided = SAT.testPolygonPolygon(airplanePoly, 
   obstacle, response);
-  if (collided) { 
+  if (collided) {
     document.getElementById("two").textContent =
-    "the plane was hit and the game is over";
-    explosionX = planeX + 25;
-    explosionY = planeY + 25;
+      "the plane was hit and the game is over";
+    explosionX = planeX + positionExplosionSwapPlanePlace;
+    explosionY = planeY + positionExplosionSwapPlanePlace;
     explosionId.style.left = explosionX + "px";
     explosionId.style.top = explosionY + "px";
     explosionCollision();
     ++hit;
   }  
 }
+
 
